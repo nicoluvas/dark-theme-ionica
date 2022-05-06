681512b2b654b8be6584325ace122882b2c4f8a0
nicolas <79096051+nicoluvas@users.noreply.github.com>
2022-05-05 22:01:07 -0300

    website update fix

--- injection.js
+++ injection.js
@@ -1,5 +1,10 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-            console.log(current_tab_info.url)
-        });
-});
+/*const anuncio = document.getElementById(".EiQDe");
+anuncio.remove();*/
+
+function removeElementsByClass(className) {
+    const elements = document.getElementsByClassName('.EiQDe');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
