fe267f8cd8fad75f78ac7efb3cd7eb2655663837
nicolas <79096051+nicoluvas@users.noreply.github.com>
2022-05-07 20:25:47 -0300

    ad removal ,2

--- injection.js
+++ injection.js
@@ -1,10 +1,9 @@
-/*const anuncio = document.getElementById(".EiQDe");
-anuncio.remove();*/
-
-const pathName = window.location.pathname;
+/*const pathName = window.location.pathname;
 if (pathName.startsWith('/ionica-platform-fe')) {
-    const anuncio = document.getElementsByClassName('sc-DJfgX')[0];
+    const anuncio = document.getElementsByClassName('sc-licaXj')[0];
     anuncio.parentNode.removeChild(anuncio);
-}
+}*/
 
-
+const anuncio = '.sc-licaXj.gZxcXm';
+console.log(anuncio);
+document.querySelectorAll(anuncio).forEach((element) => element.remove());
